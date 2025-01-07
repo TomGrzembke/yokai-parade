310f83b3a0e7819d82bac4eb3a98cdcda27664ca
Christian Schneider <fuzzyherbivore@icloud.com>
2025-01-07 20:21:02 +0100

    Changed types of enemies to RigidBody2Ds, cleanup.

--- levels/level.gd
+++ levels/level.gd
@@ -1,9 +1,10 @@
-extends Marker2D
+extends Node2D
 
 
 func _ready() -> void:
 	var player_scene = preload("res://player/player.tscn")
 	
 	var player = player_scene.instantiate()
-	player.transform = transform
+	player.transform = %PlayerSpawnPoint.transform
+	
 	get_parent().add_child.call_deferred(player)
