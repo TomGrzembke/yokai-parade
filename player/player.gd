de72430d4ec3f7c9b4d4c6f704423f608849d835
Christian Schneider <fuzzyherbivore@icloud.com>
2025-01-07 22:52:05 +0100

    Gave player a catch radius area for enemies.

--- player/player.gd
+++ player/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 @export var speed = 300.0
 @export var jump_velocity = 600.0
 
+var body_in_catch_radius = null
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -23,3 +25,19 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+
+
+func _on_catch_radius_body_entered(body: Node2D) -> void:
+	body_in_catch_radius = body
+
+
+func _on_catch_radius_body_exited(body: Node2D) -> void:
+	if body == body_in_catch_radius:
+		body_in_catch_radius = null
+
+
+func _unhandled_input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("abililty") \
+	and body_in_catch_radius != null \
+	and body_in_catch_radius.has_method("caught"):
+		body_in_catch_radius.caught()
