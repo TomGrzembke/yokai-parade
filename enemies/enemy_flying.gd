25d3fbe921bd99422c9fe5cd603ac48d106b5ca1
Christian Schneider <christian.schneider@school4games.net>
2025-01-23 17:20:28 +0100

    Added stun and recovery to enemies by including an ugly state machine
    --HG--
    branch : enemy_behavior

--- enemies/enemy_flying.gd
+++ enemies/enemy_flying.gd
@@ -1,11 +1,20 @@
 extends PathFollow2D
 
+enum EnemyState {
+	IDLING = 100,
+	MOVING = 200,
+	RECOVERING = 300,
+}
+
 signal enemy_caught
 
+@export var starting_state: EnemyState = EnemyState.MOVING
+@export var recovery_time = 3.0
 @export var easing_curve: Curve
 @export var max_speed = 200.0
 @export var element_type: EnemyElementType
 
+var state
 var speed = 0.0
 var path_length = 0.0
 var is_path_closed = false
@@ -14,6 +23,7 @@ var progress_ratio_raw = 0.0
 
 
 func _ready():
+	state = starting_state
 	check_is_path_closed()
 	speed = max_speed
 	path_length = get_parent().curve.get_baked_length()
@@ -21,7 +31,31 @@ func _ready():
 		%MeshInstance2D.modulate = element_type.get_color()
 
 
+func check_is_path_closed():
+	var path = get_parent()
+	if path == null \
+	and path:
+		return
+
+	if path.curve.get_point_position(0) == path.curve.get_point_position(path.curve.point_count - 1):
+		is_path_closed = true
+	else:
+		is_path_closed = false
+
+
 func _physics_process(delta):
+	match state:
+		EnemyState.IDLING:
+			process_idling(delta)
+		EnemyState.MOVING:
+			process_moving(delta)
+
+
+func process_idling(delta):
+	pass
+
+
+func process_moving(delta):
 	var step_ratio = (speed / path_length) * delta * progression_direction
 	progress_ratio_raw += step_ratio
 	if not is_path_closed:
@@ -43,19 +77,44 @@ func _physics_process(delta):
 		progress_ratio = progress_ratio_raw
 
 
-func check_is_path_closed():
-	var path = get_parent()
-	if path == null \
-	and path:
-		return
+func enter_state(new_state):
+	match new_state:
+		EnemyState.IDLING:
+			enter_state_idling()
+		EnemyState.MOVING:
+			enter_state_moving()
+		EnemyState.RECOVERING:
+			enter_state_recovering()
 
-	if path.curve.get_point_position(0) == path.curve.get_point_position(path.curve.point_count - 1):
-		is_path_closed = true
-	else:
-		is_path_closed = false
+
+func enter_state_idling():
+	set_alpha(1.0)
+	state = EnemyState.IDLING
+
+
+func enter_state_moving():
+	set_alpha(1.0)
+	state = EnemyState.MOVING
+
+
+func enter_state_recovering():
+	var last_state = state
+	set_alpha(0.1)
+	state = EnemyState.RECOVERING
+	var recovery_timer = get_tree().create_timer(recovery_time)
+	recovery_timer.timeout.connect(enter_state.bind(last_state))
+
+
+func set_alpha(alpha):
+	var color = %MeshInstance2D.modulate
+	color.a = alpha
+	%MeshInstance2D.modulate = color
 
 
 func got_caught():
+	if state == EnemyState.RECOVERING:
+		return null
+
 	enemy_caught.emit()
-	# TODO: Stun enemy, start timer for recovery
+	enter_state(EnemyState.RECOVERING)
 	return element_type.spawning_ability
--- enemies/enemy_grounded.gd
+++ enemies/enemy_grounded.gd
@@ -1,5 +1,11 @@
 extends CharacterBody2D
 
+enum EnemyState {
+	IDLING = 100,
+	MOVING = 200,
+	RECOVERING = 300,
+}
+
 signal enemy_caught
 
 enum Direction {
@@ -7,14 +13,18 @@ enum Direction {
 	RIGHT = 1,
 }
 
+@export var starting_state: EnemyState = EnemyState.MOVING
+@export var recovery_time = 3.0
 @export var speed = 100.0
 @export var initial_direction = Direction.RIGHT
 @export var element_type: EnemyElementType
 
+var state
 var direction
 
 
 func _ready():
+	state = starting_state
 	direction = initial_direction
 	if element_type != null:
 		%MeshInstance2D.modulate = element_type.get_color()
@@ -41,7 +51,10 @@ func handle_turn():
 
 
 func handle_movement(_delta):
-	velocity.x = direction * speed
+	match state:
+		EnemyState.MOVING:
+			velocity.x = direction * speed
+		_: velocity.x = 0.0
 
 
 func flip_horizontally():
@@ -52,12 +65,44 @@ func flip_horizontally():
 func is_on_cliff():
 	return not %RayCast2D.is_colliding()
 
+func enter_state(new_state):
+	match new_state:
+		EnemyState.IDLING:
+			enter_state_idling()
+		EnemyState.MOVING:
+			enter_state_moving()
+		EnemyState.RECOVERING:
+			enter_state_recovering()
 
-func on_despawn():
-	queue_free()
+
+func enter_state_idling():
+	set_alpha(1.0)
+	state = EnemyState.IDLING
+
+
+func enter_state_moving():
+	set_alpha(1.0)
+	state = EnemyState.MOVING
+
+
+func enter_state_recovering():
+	var last_state = state
+	set_alpha(0.1)
+	state = EnemyState.RECOVERING
+	var recovery_timer = get_tree().create_timer(recovery_time)
+	recovery_timer.timeout.connect(enter_state.bind(last_state))
+
+
+func set_alpha(alpha):
+	var color = %MeshInstance2D.modulate
+	color.a = alpha
+	%MeshInstance2D.modulate = color
 
 
 func got_caught():
+	if state == EnemyState.RECOVERING:
+		return null
+
 	enemy_caught.emit()
-	# TODO: Stun enemy, start timer for recovery
+	enter_state(EnemyState.RECOVERING)
 	return element_type.spawning_ability
