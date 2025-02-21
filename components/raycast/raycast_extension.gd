7c3d6185f83f3a3a6ec71fbc08a4f5c19e13f327
TomGrzembke <grzembketom@gmail.com>
2025-02-21 17:27:02 +0000

    lookat uses length now

--- components/raycast/raycast_extension.gd
+++ components/raycast/raycast_extension.gd
@@ -1,17 +1,20 @@
 extends RayCast2D
 
-var initial_offset
+var initial_length
 
 func _ready():
-	initial_offset = target_position.x
+	initial_length = target_position.length()
+
 
 func has_target():
 	return is_colliding()
+
 
 func get_target():
 	if !has_target(): return null
 	return get_collider()
 
+
 func lookat_direction(target_pos):
 	var look_pos = to_local(target_pos)
-	target_position = look_pos.normalized() * initial_offset
+	target_position = look_pos.normalized() * initial_length
