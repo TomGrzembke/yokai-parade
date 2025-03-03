4fdb235af14a6e9367188201bc5580609e40a427
Christian Schneider <christian.schneider@school4games.net>
2025-03-03 21:46:51 +0100

    Updated options screen to use a grid container; Improved audio sliders for effects to play sounds when being changed, with a cool-down timer to avoid sound spam

--- options/options.gd
+++ options/options.gd
@@ -5,6 +5,9 @@ signal window_fullscreen_changed(active)
 signal volume_audio_bus_changed(bus_id, volume_db)
 signal back_button_pressed()
 
+var is_dragging = false
+var is_cooling_down = false
+
 
 func _ready():
 	%FullscreenCheckBox.toggled.connect(func (active): window_fullscreen_changed.emit(active))
@@ -16,16 +19,16 @@ func _ready():
 		func (volume_linear): volume_audio_bus_changed.emit(1, volume_linear)
 	)
 	%VolumeSfxSlider.value_changed.connect(
-		func (volume_linear): volume_audio_bus_changed.emit(2, volume_linear)
+		func (volume_linear): change_effects_volume(2, volume_linear, %SfxAudioStreamPlayer)
 	)
 	%VolumeUiSlider.value_changed.connect(
-		func (volume_linear): volume_audio_bus_changed.emit(3, volume_linear)
+		func (volume_linear): change_effects_volume(3, volume_linear, %UiAudioStreamPlayer)
 	)
 
-	%VolumeSfxSlider.drag_started.connect(loop_sfx_sound)
+	%VolumeSfxSlider.drag_started.connect(drag_sfx_sound)
 	%VolumeSfxSlider.drag_ended.connect(stop_sound_loop)
 
-	%VolumeUiSlider.drag_started.connect(loop_ui_sound)
+	%VolumeUiSlider.drag_started.connect(drag_ui_sound)
 	%VolumeUiSlider.drag_ended.connect(stop_sound_loop)
 
 	%BackButton.pressed.connect(func (): back_button_pressed.emit())
@@ -52,13 +55,31 @@ func update_volume_ui_slider(volume_linear):
 	%VolumeUiSlider.set_value_no_signal(volume_linear)
 
 
-func loop_sfx_sound():
+func change_effects_volume(bus_id, volume_linear, audio_player):
+	volume_audio_bus_changed.emit(bus_id, volume_linear)
+
+	if not is_dragging \
+	and not is_cooling_down:
+		audio_player.play()
+		is_cooling_down = true
+
+	get_tree().create_timer(0.125).timeout.connect(end_cooling_down)
+
+
+func end_cooling_down():
+	is_cooling_down = false
+
+
+func drag_sfx_sound():
+	is_dragging = true
 	%AnimationPlayer.play("loop_sfx_sound")
 
 
-func loop_ui_sound():
+func drag_ui_sound():
+	is_dragging = true
 	%AnimationPlayer.play("loop_ui_sound")
 
 
 func stop_sound_loop(_changed):
+	is_dragging = false
 	%AnimationPlayer.stop()
