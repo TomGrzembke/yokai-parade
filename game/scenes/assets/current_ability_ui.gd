064f4ff0b5e7d43b65dba73a4bed26d82b9ad497
TomGrzembke <grzembketom@gmail.com>
2025-03-07 17:34:36 +0000

    Build UI color blending

--- game/scenes/assets/current_ability_ui.gd
+++ game/scenes/assets/current_ability_ui.gd
@@ -1,20 +1,50 @@
-extends TextureRect
+extends Control
 
 const ELEMENTS = preload("res://elements/elements.gd")
 
-var default_frame
-@export var air_frame : Texture2D
-@export var fire_frame : Texture2D
+@export var blend_time : float = .5
+@export var air_frame : TextureRect
+@export var fire_frame : TextureRect
 
-
-func _ready():
-	default_frame = texture
-
+var blend_timer
 
 func change_with_color(color):
-	if color == ELEMENTS.COLOR_MAP[ELEMENTS.ElementType.AIR]:
-		texture = air_frame
-	elif color == ELEMENTS.COLOR_MAP[ELEMENTS.ElementType.FIRE]:
-		texture = fire_frame
+	if is_color_element(color, ELEMENTS.ElementType.AIR):
+		activate_rect(air_frame)
+
+	elif is_color_element(color, ELEMENTS.ElementType.FIRE):
+		activate_rect(fire_frame)
+
 	else:
-		texture = default_frame
+		blend_timer = create_timer(blend_time)
+
+
+func is_color_element(color, element):
+	return  color == ELEMENTS.COLOR_MAP[element]
+
+
+func _physics_process(_delta):
+	if  blend_timer == null || blend_timer.time_left <= 0: return
+
+	fadeout_rect(air_frame)
+	fadeout_rect(fire_frame)
+
+
+func activate_rect(rect):
+	rect.modulate = get_white_set_alpha(1)
+
+	if blend_timer == null: return
+	blend_timer.time_left = 0.0
+
+
+func fadeout_rect(rect):
+	rect.modulate = get_white_set_alpha(lerpf(rect.modulate.a, 0.0, 1.0 - blend_timer.time_left / blend_time))
+
+
+func get_white_set_alpha(alpha):
+	alpha = clamp(alpha, 0.0, 1.0)
+	return Color(1, 1, 1, alpha)
+
+
+func create_timer(time):
+	return get_tree().create_timer(time)
