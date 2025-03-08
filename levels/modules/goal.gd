15adfd8f2ed5bd85264f77e84a9ffd625d7d9078
Christian Schneider <christian.schneider@school4games.net>
2025-03-08 13:40:07 +0100

    Exchanged goal post with new animated version

--- levels/modules/goal.gd
+++ levels/modules/goal.gd
@@ -1,19 +1,10 @@
 extends Area2D
 
 
-@export var texture_inactive: Texture2D
-@export var texture_active: Texture2D
-
-
-func _ready():
-	%Sprite2D.texture = texture_inactive
-
-
 func on_body_entered(other):
 	if other == null \
 	or not other.has_method("on_goal_reached"):
 		return
 
 	other.on_goal_reached()
-	%Sprite2D.texture = texture_active
 	set_deferred("monitoring", false)
